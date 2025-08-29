42a6039641b87c01f60b917b7692a63e6a1d5222
rafiul-i02 <per.rafiuli02@gmail.com>
2025-08-29 20:54:14 +0600

    Arithmetic Operators
    - Replaced incorrect use of `and` with `or` in input validation.
    - Now the program correctly checks whether `a` or `b` is outside the allowed range [1, 10^10].
    - If either input is invalid, it prints "Not Applicable".
    - Otherwise, the program outputs the sum, difference, and product of the two inputs.

---
+++ Arithmetic Operators.py
@@ -0,0 +1,11 @@
+a=int(input())
+b=int(input())
+
+if a<1 or a>10**10:
+    print("Not Applicable")
+elif b<1 or b>10**10:
+    print("Not Applicable")
+else:
+    print(a+b)
+    print(a-b)
+    print(a*b)    
